38dd72ca220fb9f9913af08d221f06ec6e022e5d
Magnus Kulke <magnus.kulke@mobimeo.com>
2022-01-22 17:06:26 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# kube-relay
+
+Small CLI tool to proxy tcp connections from local to resources in k8s network (e.g. databases or resource in other namespaces that you cannot access directly or via `kubectl port-forward`). It will spawn a pod with `socat` to use as relay and connect to that pod via kubernetes' port-forwarding facility.
+
+## Build
+
+Tested with Go v1.17 on Linux and MacOS.
+
+```bash
+go mod tidy
+go build
+```
+
+## Run
+
+```bash
+./kube-relay -ch some-service.my-namespace
+Created pod "kube-relay"
+Pod "kube-relay" is running
+Forwarding from 127.0.0.1:1999 -> 9000
+Forwarding from [::1]:1999 -> 9000
+```
+
+Test in another shell
+
+```bash
+curl localhost:1999/health
+{"status":"OK"}
+```
---
+++ main.go
@@ -0,0 +1,188 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+	"os/signal"
+	"strings"
+
+	"github.com/urfave/cli/v2"
+	apiv1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/portforward"
+	"k8s.io/client-go/transport/spdy"
+)
+
+const POD_NAME = "kube-relay"
+const POD_IMAGE = "alpine/socat:1.7.4.2-r0"
+
+func run(localPort uint, clusterHost string, clusterPort uint) {
+	kubeconfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+		clientcmd.NewDefaultClientConfigLoadingRules(),
+		&clientcmd.ConfigOverrides{},
+	)
+
+	namespace, _, err := kubeconfig.Namespace()
+	if err != nil {
+		panic(err)
+	}
+
+	// use the current context in kubeconfig
+	config, err := kubeconfig.ClientConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	podsClient := clientset.CoreV1().Pods(namespace)
+
+	manifest := &apiv1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: POD_NAME,
+		},
+		Spec: apiv1.PodSpec{
+			Containers: []apiv1.Container{
+				{
+					Name:  "socat",
+					Image: POD_IMAGE,
+					Args: []string{
+						"TCP-LISTEN:9000,fork",
+						fmt.Sprintf("TCP:%s:%d", clusterHost, clusterPort),
+					},
+				},
+			},
+		},
+	}
+
+	cleanup := func() {
+		fmt.Printf("Delete pod %q\n", POD_NAME)
+		podsClient.Delete(context.TODO(), POD_NAME, metav1.DeleteOptions{})
+	}
+
+	result, err := podsClient.Create(context.TODO(), manifest, metav1.CreateOptions{})
+	if err != nil {
+		panic(err)
+	}
+	defer cleanup()
+	fmt.Printf("Created pod %q\n", result.GetObjectMeta().GetName())
+
+	selector := fmt.Sprintf("metadata.name=%s", POD_NAME)
+	podWatch, err := podsClient.Watch(context.TODO(), metav1.ListOptions{FieldSelector: selector})
+	if err != nil {
+		panic(err)
+	}
+
+	podReady := make(chan int, 1)
+
+	go func() {
+		for event := range podWatch.ResultChan() {
+			p, ok := event.Object.(*v1.Pod)
+			if !ok {
+				panic("unexpected type")
+			}
+			if p.Status.Phase == "Running" {
+				fmt.Printf("Pod %q is running\n", p.Name)
+				podReady <- 1
+			}
+		}
+	}()
+
+	ctrlc := make(chan os.Signal, 1)
+	signal.Notify(ctrlc, os.Interrupt)
+	go func() {
+		<-ctrlc
+		cleanup()
+		os.Exit(1)
+	}()
+
+	roundTripper, upgrader, err := spdy.RoundTripperFor(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	path := fmt.Sprintf("/api/v1/namespaces/%s/pods/%s/portforward", namespace, POD_NAME)
+	hostIP := strings.TrimLeft(config.Host, "htps:/")
+	serverURL := url.URL{Scheme: "https", Path: path, Host: hostIP}
+
+	dialer := spdy.NewDialer(upgrader, &http.Client{Transport: roundTripper}, http.MethodPost, &serverURL)
+
+	stopChan, readyChan := make(chan struct{}, 1), make(chan struct{}, 1)
+	out, errOut := new(bytes.Buffer), new(bytes.Buffer)
+
+	ports := fmt.Sprintf("%d:9000", localPort)
+	forwarder, err := portforward.New(dialer, []string{ports}, stopChan, readyChan, out, errOut)
+	if err != nil {
+		panic(err)
+	}
+
+	go func() {
+		for range readyChan { // Kubernetes will close this channel when it has something to tell us.
+		}
+		if len(errOut.String()) != 0 {
+			panic(errOut.String())
+		} else if len(out.String()) != 0 {
+			print(out.String())
+		}
+	}()
+
+	<-podReady
+	err = forwarder.ForwardPorts()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	var localPort uint
+	var clusterPort uint
+	var clusterHost string
+
+	app := &cli.App{
+		Flags: []cli.Flag{
+			&cli.UintFlag{
+				Name:        "local-port",
+				Aliases:     []string{"l"},
+				Value:       1999,
+				Usage:       "the local tcp port",
+				Destination: &localPort,
+			},
+			&cli.StringFlag{
+				Name:        "cluster-host",
+				Aliases:     []string{"ch"},
+				Usage:       "the cluster host",
+				Destination: &clusterHost,
+				Required:    true,
+			},
+			&cli.UintFlag{
+				Name:        "cluster-port",
+				Aliases:     []string{"cp"},
+				Value:       80,
+				Usage:       "the cluster tcp port",
+				Destination: &clusterPort,
+			},
+		},
+		Name:  "kube-relay",
+		Usage: "access tcp ports in a kubernetes cluster via a pod relay (locally)",
+		Action: func(c *cli.Context) error {
+			run(localPort, clusterHost, clusterPort)
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		panic(err)
+	}
+}
